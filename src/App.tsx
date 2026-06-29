890bc7fc4ba05e2252f01fe27c5a0b74e1d53f1f
manuNamuncura <manu.namun94@gmail.com>
2026-06-29 12:07:08 -0300

    feat: mejor tiempo de carga

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
